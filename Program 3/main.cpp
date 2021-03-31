67ea1402d4bbbae0a6b12dcec6a6c018b2e29993
JD Villarroel <62124023+jdvillarroel@users.noreply.github.com>
2021-03-30 19:43:43 -0500

    Add files via upload

--- Program 3/main.cpp
+++ Program 3/main.cpp
@@ -39,34 +39,98 @@ using namespace std;
 const   int     ROWS = 3;
 const   int     COLS = 3;
 
+// Player 1 = false / Player 2 = true
+bool    playerTurn = false;
+
+// Symbols used by players
+char    p1Symbol = 'X';
+char    p2Symbol = 'O';
+
+// Define "empty" board.
 char    board[ROWS][COLS] = {
     {'*', '*', '*'},
     {'*', '*', '*'},
     {'*', '*', '*'}
 };
 
+// Function prototypes.
 void initialMsg(void);
 void displayBoard(void);
 void getUserInput(void);
 void updateBoard(int, int);
+void resetBoard(void);
+bool checkIfWon(char);
 
 int main()
 {
     char    repeat = 'n';
+    bool    didWin = false;
+    string  winner = "";
 
     // Display initial message.
-    initialMsg();
+    initialMsg();    
 
-    displayBoard();
-
+    // main loop.
     do
     {
+        // Reset board. Its purpose its handy when loop repeats.
+        resetBoard();
+
+        // Display "empty" board.
+        displayBoard();
+
+        // Set turn to player one.
+        playerTurn = false;
+
+
         for (int turn = 0; turn < 9; turn++)
         {
+            // Get user input, validate it, and update board.
             getUserInput();
+
+            // Clear screen and display updated board.
+            system("cls");
+            displayBoard();
+            
+            // Check if user won.
+            if (playerTurn)
+            {
+                didWin = checkIfWon(p2Symbol);
+
+                if (didWin)
+                {
+                    winner = "player 2";
+                    break;
+                }
+            }                
+            else
+            {
+                didWin = checkIfWon(p1Symbol);
+
+                if (didWin)
+                {
+                    winner = "player 1";
+                    break;
+                }
+            }
+                
+            // Switch turn to next player.
+            playerTurn = !playerTurn;
+
+            
         }
 
+        // If there's a winner display message or tie game message.
+        if (didWin)
+        {
+            cout << "\nCONGRATULATIONS!! " << winner << ". You did it!" << endl << endl;
+        }
+        else
+        {
+            cout << "\nGame draw!!" << endl << endl;
+        }
 
+        // Prompt user to play again.
         cout << "\nWould you like to play again? (y/n): ";
         cin >> repeat;
         cin.ignore(100, '\n');
@@ -98,26 +162,86 @@ void displayBoard(void)
     cout << " " << board[2][0] << " | " << board[2][1] << " | " << board[2][2] << endl;
 }
 
+void resetBoard(void)
+{
+    for (int i = 0; i < ROWS; i++)
+        for (int j = 0; j < COLS; j++)
+            board[i][j] = '*';
+}
+
 void getUserInput(void)
 {
     int     x = 0,
             y = 0;
 
-    cout << "Please, enter your selection (row and column): ";
+    cout << "\nPlease, enter your selection (row \"space\" column): ";
     cin >> x;
     cin >> y;
     cin.ignore(100, '\n');
 
-    while ((x < 1 || x > 3) || ((y < 1 || y > 3)))
+    // Validate input is in range.
+    while (x < 1 || x > 3 || y < 1 || y > 3)
     {
         cout << "\nSelection out of range.\n\n";
         cout << "Please, enter your selection (row and column): ";
-        cin >> x, y;
+        cin >> x;
+        cin >> y;
         cin.ignore(100, '\n');
     }
+
+    // Check that input has not been played previously.
+    while (board[x - 1][y - 1] != '*')
+    {
+        cout << "\nCell already selected.\n\n";
+        cout << "Please, enter your selection (row and column): ";
+        cin >> x;
+        cin >> y;
+        cin.ignore(100, '\n');
+        cout << endl;
+    }
+
+    // Update board with player selection.
+    updateBoard(x, y);
 
 }
 
 void updateBoard(int row, int col)
 {
+    if (playerTurn)
+        board[row - 1][col - 1] = p2Symbol;
+    else
+        board[row - 1][col - 1] = p1Symbol;
+
+}
+
+bool checkIfWon(char playerSymbol)
+{
+    // Check rows.
+    if (board[0][0] == playerSymbol && board[0][1] == playerSymbol && board[0][2] == playerSymbol)
+        return true;
+
+    if (board[1][0] == playerSymbol && board[1][1] == playerSymbol && board[1][2] == playerSymbol)
+        return true;
+
+    if (board[2][0] == playerSymbol && board[2][1] == playerSymbol && board[2][2] == playerSymbol)
+        return true;
+
+    // Check columns.
+    if (board[0][0] == playerSymbol && board[1][0] == playerSymbol && board[2][0] == playerSymbol)
+        return true;
+
+    if (board[0][1] == playerSymbol && board[1][1] == playerSymbol && board[2][1] == playerSymbol)
+        return true;
+
+    if (board[0][2] == playerSymbol && board[1][2] == playerSymbol && board[2][2] == playerSymbol)
+        return true;
+
+    // Check diagonals.
+    if (board[0][0] == playerSymbol && board[1][1] == playerSymbol && board[2][2] == playerSymbol)
+        return true;
+
+    if (board[0][2] == playerSymbol && board[1][1] == playerSymbol && board[2][0] == playerSymbol)
+        return true;
+
+    return false;
 }
